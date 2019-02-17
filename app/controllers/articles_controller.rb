48e5e3b9d6c67a3b985e19b115c06880073345d4
Jorgelin Ortiz <jorgar2000@gmail.com>
2019-02-17 19:16:16 -0400

    Base project

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,19 @@
+class ArticlesController < ApplicationController
+  def new
+  end
+
+  def index
+  	@articles = Article.all
+  end
+
+  def show
+  	@article = Article.find(params[:id])
+  end
+
+  def create
+  	@article = Article.new(params.require(:article).permit(:title, :text))
+
+  	@article.save
+  	redirect_to @article
+  end
+end
