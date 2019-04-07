0592d0b7c17d827a8e9e7034366dbd34957c27e0
Jorgelin Ortiz <jorgar2000@gmail.com>
2019-04-06 20:36:41 -0400

    Implementacion de CRUD para modelo de ejemplo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,9 +11,38 @@ class ArticlesController < ApplicationController
   end
 
   def create
-  	@article = Article.new(params.require(:article).permit(:title, :text))
-
-  	@article.save
-  	redirect_to @article
+    @article = Article.new(article_params)
+   
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
+   
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+   
+    redirect_to articles_path
+  end
+   
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
